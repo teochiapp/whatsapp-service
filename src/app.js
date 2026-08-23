cb89e35a3f94a8e3adb36cfbebda94cfab17c746
Teo <teochiapps@gmail.com>
2026-08-22 23:41:58 -0300

    feat: auto-inject token in test client

--- src/app.js
+++ src/app.js
@@ -11,9 +11,14 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+const fs = require('fs');
+
 // Serve the test client on the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../test-client.html'));
+    const htmlPath = path.join(__dirname, '../test-client.html');
+    let html = fs.readFileSync(htmlPath, 'utf8');
+    html = html.replace('REPLACE_WITH_YOUR_TOKEN', process.env.WHATSAPP_INTERNAL_TOKEN || '');
+    res.send(html);
 });
 
 // Initialize WhatsApp on startup
